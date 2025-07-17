abdbcf650ce7161f3d70ac11e1c9727e2047a3bd
GabiiCarvalho <gabriele.carvalho.16@icloud.com>
2025-07-17 20:11:20 -0300

    ultimo ajuste back

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 const app = express();
 
 const corsOptions = {
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://ng-express.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
   credentials: true,
